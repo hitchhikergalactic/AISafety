5d9a4e4296fd69ff917b4c102df061774fdf4f95
briveraarg <brivera@student.42madrid.com>
2026-05-29 10:48:25 +0200

    incorporacion video

--- src/pages/QueHacemos.tsx
+++ src/pages/QueHacemos.tsx
@@ -51,6 +51,22 @@ const QueHacemos: React.FC<QueHacemosProps> = ({ lang, setLang, theme, setTheme 
               {parseText(content.impactDescription)}
             </p>
           </section>
+
+          {/* Video Section */}
+          <section className="mb-20 md:mb-16">
+            <div className="relative w-full aspect-video rounded-md overflow-hidden">
+              <iframe
+                className="absolute top-0 left-0 w-full h-full"
+                src="https://www.youtube.com/embed/SCLpMS6f1CE?si=CHZd9bh_TECN4exW"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              />
+            </div>
+          </section>
+
           <section className="mb-20 md:mb-52"></section>
         </div>
       </main>
